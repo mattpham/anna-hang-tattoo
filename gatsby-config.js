e6994e5397dee008dde3586d01e21ecbbe56bc03
matt pham <matt@dreymur.com>
2018-01-10 09:45:25 -0800

    Add typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Default Starter`
   },
   pathPrefix: '/tattoo-site',
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`
+      }
+    }
+  ]
+};
