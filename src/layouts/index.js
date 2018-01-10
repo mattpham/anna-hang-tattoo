e1cc1d4c55cef7859124455ae398afdcb5c91391
matt pham <matt@dreymur.com>
2018-01-10 14:38:26 -0800

    Add menu links

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,63 +1,83 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
 
+const ListItem = (props) => (
+  <li
+    style={{
+      display: 'inline-block',
+      marginRight: '1rem'
+    }}
+  >
+    <Link
+      to="/"
+      >
+    {props.children}
+    </Link>
+  </li>
+);
 
 const Header = () => (
-  <div
+  <header
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      // background: 'rebeccapurple',
+      marginBottom: '1.45rem'
     }}
   >
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: '1.45rem 1.0875rem'
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <h1 style={{ display: 'inline'}}>
         <Link
           to="/"
           style={{
-            color: 'white',
-            textDecoration: 'none',
+            // color: 'white',
+            textDecoration: 'none'
           }}
         >
-          Gatsby
+          Tattoo Site
         </Link>
       </h1>
+      <ul style={{ listStyle: 'none', float: 'right' }}>
+        <ListItem>Home</ListItem>
+        <ListItem>About</ListItem>
+        <ListItem>Gallery</ListItem>
+        <ListItem>Contact</ListItem>
+      </ul>
     </div>
-  </div>
-)
+  </header>
+);
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Tattoo Site"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Tattoo' },
+        { name: 'keywords', content: 'tattoo, art' }
       ]}
     />
     <Header />
-    <div
+    <main
       style={{
         margin: '0 auto',
         maxWidth: 960,
         padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        paddingTop: 0
       }}
     >
       {children()}
-    </div>
+    </main>
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
