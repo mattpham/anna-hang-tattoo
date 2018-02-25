6117717eebf1c9d5e8e15647301b995028423197
matt pham <matt@dreymur.com>
2018-02-25 10:13:11 -0800

    Add gatsby styled component plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   pathPrefix: '/tattoo-site',
   plugins: [
     'gatsby-plugin-react-next',
+    'gatsby-plugin-styled-components',
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
